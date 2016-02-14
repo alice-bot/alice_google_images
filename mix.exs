bd874dad360e6f2fa982947a11d8e232ca2f59cd
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2016-02-14 04:27:52 -0600

    updates mix.exs for publishing to hex

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule AliceGoogleImages.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "A handler for the Alice Slack bot. Get random images from Google",
+     package: package,
      deps: deps]
   end
 
@@ -19,5 +21,13 @@ defmodule AliceGoogleImages.Mixfile do
       {:websocket_client, github: "jeremyong/websocket_client"},
       {:alice, "~> 0.1.0"}
     ]
+  end
+
+  defp package do
+    [files: ["lib", "config", "mix.exs", "README*"],
+     maintainers: ["Adam Zaninovich"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/adamzaninovich/alice_google_images",
+              "Docs"   => "https://github.com/adamzaninovich/alice_google_images"}]
   end
 end
