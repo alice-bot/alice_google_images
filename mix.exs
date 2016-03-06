8e45509a077109da21aea1f6a465bbb20ba626c3
Devin Clark <notdevinclark@gmail.com>
2016-03-05 20:59:37 -0600

    bumps version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AliceGoogleImages.Mixfile do
 
   def project do
     [app: :alice_google_images,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
