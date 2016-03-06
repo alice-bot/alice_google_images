127a33207e102bc600e907cd5a7f943c017f4518
Devin Clark <notdevinclark@gmail.com>
2016-03-05 20:59:02 -0600

    updates and refactors the google_images module with gif/animate options
    adds the command and route for gif/animate
    refactors how query params are instantiated and pulls them into the
    pipe flow of fetch function
    refactors get_images to take params instead of the term
    updates tests with the new changes

--- test/alice/handlers/google_images_test.exs
+++ test/alice/handlers/google_images_test.exs
@@ -28,23 +28,26 @@ defmodule Alice.Handlers.GoogleImagesTest do
   end
 
   test "extract_term gets the term" do
-    {pattern, :fetch} = hd(GI.routes)
+    {:ok, pattern} = GI.routes
+                     |> Enum.map(fn({p,n}) -> {n,p} end)
+                     |> Keyword.fetch(:fetch_image)
+
     conn = Alice.Conn.make(%{text: "img me stuff"}, :slack, :state)
            |> Alice.Conn.add_captures(pattern)
     assert GI.extract_term(conn) == "stuff"
   end
 
   test "get_images returns the response" do
-    assert {:ok, :body} = GI.get_images("good result")
+    assert {:ok, :body} = GI.get_images(q: "good result")
   end
 
   test "get_images returns an error when there is an error" do
-    assert {:error, :reason} = GI.get_images("bad result")
+    assert {:error, :reason} = GI.get_images(q: "bad result")
   end
 
   test "get_images calls HTTPoison get with the correct options" do
     url = "https://www.googleapis.com/customsearch/v1"
-    GI.get_images("mock result")
+    GI.get_images(q: "mock result")
 
     assert_received {:mock_response, {^url, params}}
     assert "mock result" = params[:q]
